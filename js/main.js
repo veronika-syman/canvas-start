10f95ea5915b9949590f12a6d7ce80dc5c42a54f
veronika-syman <116827488+veronika-syman@users.noreply.github.com>
2023-04-13 16:55:41 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,300 @@
+'use strict'
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = '#003399';
+ctx.fillRect(0, 0, 800, 600);
+
+ctx.fillStyle = '#ffffff';
+ctx.fillRect(0, 400, 800, 200);
+
+
+ctx.fillStyle = '#c0c0c0';
+ctx.fillRect(20, 200, 150, 300);
+
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(26, 230, 42, 40);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(74, 230, 42, 40);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(122, 230, 42, 40);
+
+
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(26, 280, 42, 40);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(74, 280, 42, 40);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(122, 280, 42, 40);
+
+
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(26, 330, 42, 40);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(74, 330, 42, 40);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(122, 330, 42, 40);
+
+
+
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(26, 380, 42, 40);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(74, 380, 42, 40);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(122, 380, 42, 40);
+
+
+
+
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(26, 430, 42, 40);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(74, 430, 42, 65);
+
+ctx.fillStyle = '#000000';
+ctx.fillRect(122, 430, 42, 40);
+
+
+ctx.beginPath();
+ctx.arc(310, 410, 40, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(310, 352, 33, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+
+ctx.beginPath();
+ctx.arc(310, 305, 25, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(270, 350, 15, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(350, 350, 15, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(320, 300, 4, 0, Math.PI * 2);
+ctx.fillStyle = '#000000';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(300, 300, 4, 0, Math.PI * 2);
+ctx.fillStyle = '#000000';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+
+ctx.beginPath();
+ctx.arc(300, 320, 2, 0, Math.PI * 2);
+ctx.fillStyle = '#000000';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(304, 321, 2, 0, Math.PI * 2);
+ctx.fillStyle = '#000000';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(308, 322, 2, 0, Math.PI * 2);
+ctx.fillStyle = '#000000';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(312, 322, 2, 0, Math.PI * 2);
+ctx.fillStyle = '#000000';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(316, 321, 2, 0, Math.PI * 2);
+ctx.fillStyle = '#000000';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.arc(320, 320, 2, 0, Math.PI * 2);
+ctx.fillStyle = '#000000';
+ctx.fill();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+ctx.beginPath();
+ctx.fillStyle = '#ff8c00';
+ctx.moveTo(310, 310);
+ctx.lineTo(270, 315);
+ctx.lineTo(310, 315);
+ctx.lineTo(310, 310);
+ctx.closePath();
+ctx.fill()
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+
+
+
+ctx.fillStyle = '#8b4513';
+ctx.fillRect(600, 390, 30, 60);
+
+
+ctx.beginPath();
+ctx.fillStyle = '#00ff00';
+ctx.moveTo(540, 390);
+ctx.lineTo(615, 320);
+ctx.lineTo(690, 390);
+ctx.lineTo(540, 390);
+ctx.closePath();
+ctx.fill()
+
+
+ctx.beginPath();
+ctx.fillStyle = '#00ff00';
+ctx.moveTo(550, 330);
+ctx.lineTo(615, 260);
+ctx.lineTo(680, 330);
+ctx.lineTo(550, 330);
+ctx.closePath();
+ctx.fill()
+
+
+
+ctx.beginPath();
+ctx.fillStyle = '#00ff00';
+ctx.moveTo(560, 270);
+ctx.lineTo(615, 210);
+ctx.lineTo(670, 270);
+ctx.lineTo(560, 270);
+ctx.closePath();
+ctx.fill()
+
+
+ctx.beginPath();
+ctx.fillStyle = '#00ff00';
+ctx.moveTo(570, 220);
+ctx.lineTo(615, 180);
+ctx.lineTo(660, 220);
+ctx.lineTo(570, 220);
+ctx.closePath();
+ctx.fill()
+
+
+ctx.beginPath();
+ctx.ellipse(100, 60, 60, 30, 0, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(130, 60, 45, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+
+ctx.beginPath();
+ctx.ellipse(155, 60, 60, 30, 0, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+
+
+
+ctx.beginPath();
+ctx.ellipse(310, 120, 60, 30, 0, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(340, 120, 45, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+
+ctx.beginPath();
+ctx.ellipse(365, 120, 60, 30, 0, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+
+
+
+ctx.beginPath();
+ctx.ellipse(520, 60, 60, 30, 0, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(550, 60, 45, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
+
+ctx.beginPath();
+ctx.ellipse(575, 60, 60, 30, 0, 0, Math.PI * 2);
+ctx.fillStyle = '#ffffff';
+ctx.fill();
